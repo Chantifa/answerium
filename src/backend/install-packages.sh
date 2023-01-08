49b35395b3408d77398dd03c2059a9768bf84297
chantifa <chantifa@windowslive.com>
2023-01-08 14:18:34 +0100

    docker && prometheus && grafana

--- src/backend/install-packages.sh
+++ src/backend/install-packages.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+./bin/bash
 # Bash "strict mode", to help catch problems and bugs in the shell
 # script. Every bash script you write should include this. See
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/ for
